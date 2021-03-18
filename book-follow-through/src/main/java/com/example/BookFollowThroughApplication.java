e28383976ef3f0358a73170d4592a493f17d55c2
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-03-18 14:40:39 +0800

    init

---
+++ book-follow-through/src/main/java/com/example/BookFollowThroughApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFollowThroughApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookFollowThroughApplication.class, args);
+    }
+
+}
